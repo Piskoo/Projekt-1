685390ba1aa23ac7d1e67a2acd997d06c1cf3f90
Pisko <pisko1211@wp.pl>
2016-11-30 14:11:31 +0100

    Main game board

--- Snake/Snake/Snake.cpp
+++ Snake/Snake/Snake.cpp
@@ -1,11 +1,34 @@
-// Snake.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
+#include <iostream>
+#include <conio.h>
+	
+	
+const int TabWidth = 25; // Board width
+const int TabHeight = 25; // Board height
 
 
-int main()
+int main() 
 {
+	// Main game board
+
+	for (int i = 0; i < TabWidth; i++) { // Top border
+		std::cout << "--";
+	}
+
+	for (int i = 0; i < TabHeight; i++) { // Left border
+		std::cout << std::endl << "||";
+		for (int j = 0; j < TabWidth-2; j++) { // Space between borders
+			std::cout << "  ";
+		}
+		std::cout << "||"; // Right border
+	}
+	std::cout << std::endl;
+	for (int i = 0; i < TabWidth; i++) { // Bottom border
+		std::cout << "--";
+	}
+
+	_getch();
+
     return 0;
 }
 
